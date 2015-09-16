cd43b4577af485f68a1936cac55d7fe1b1554dec
clayton easterbrooks & cristoph pirringer <student@epicodus.com>
2015-09-16 16:00:39 -0700

    refactored code for turnCounter and displaying the current turn graphically

--- js/jquery-scripts.js
+++ js/jquery-scripts.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
 
-
   var game = new Game();
+
   $(".space").each(function() {
     $(this).click(function(event){
       var space_id = this.id;
-      // debugger;
       game.gameAction(game.gameBoard.spaces[space_id]);
       $(this).append(game.gameBoard.spaces[space_id].occupiedBy);
       $(this).off()
       $("#info_bar").append("Player:" + game.turnCounter + ", Move: " + game.gameBoard.spaces[space_id].occupiedBy + game.winCheck())
-// debugger;
-      var Player = "PlayerO";
-      if (game.turnCounter === -1){
-        Player = "PlayerX";
-        $("#playerX").text("X");
-        $("#playerO").text("")
-      } else {
-        $("#playerO").text("O");
-        $("#playerX").text("")
-      }
+
       if (game.winCheck() === true) {
         $("#game_status").text("You won " + Player + "!");
+        $("#playerX").text("");
+        $("#playerO").text("");
+        $(".space").off();
       }
+
+      Player = "PlayerX";
+      if (game.turnCounter === -1 && game.winCheck() == false){
+        Player = "PlayerO";
+        $("#playerO").text("O's turn!");
+        $("#playerX").text("");
+      } else if(game.turnCounter === 1 && game.winCheck() == false){
+        $("#playerX").text("X's turn!");
+        $("#playerO").text("");
+      } else {}
+
+
 
     });
   });
