9843c4312b86c1aac73a76c7719ea407c7067a4b
clayton easterbrooks & cristoph pirringer <student@epicodus.com>
2015-09-16 15:32:58 -0700

    only allow a space to be claimed once, display current turn and winner if game ends in a win

--- js/jquery-scripts.js
+++ js/jquery-scripts.js
@@ -8,7 +8,22 @@ $(document).ready(function(){
       // debugger;
       game.gameAction(game.gameBoard.spaces[space_id]);
       $(this).append(game.gameBoard.spaces[space_id].occupiedBy);
-      $("#info_bar").append("Player:" + game.turnCounter + ", Move: " + game.gameBoard.spaces[space_id].occupiedBy)
+      $(this).off()
+      $("#info_bar").append("Player:" + game.turnCounter + ", Move: " + game.gameBoard.spaces[space_id].occupiedBy + game.winCheck())
+// debugger;
+      var Player = "PlayerO";
+      if (game.turnCounter === -1){
+        Player = "PlayerX";
+        $("#playerX").text("X");
+        $("#playerO").text("")
+      } else {
+        $("#playerO").text("O");
+        $("#playerX").text("")
+      }
+      if (game.winCheck() === true) {
+        $("#game_status").text("You won " + Player + "!");
+      }
+
     });
   });
 });
